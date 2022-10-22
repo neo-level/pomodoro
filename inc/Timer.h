26a36088d2549d9a046ed23373d421e563d1c371
neo-level <frederik@neo-level.com>
2022-10-22 19:37:40 +0200

    new timer class to move separate pomodoro stuff with time based stuff

---
+++ inc/Timer.h
@@ -0,0 +1,10 @@
+//
+// Created by Frederik Desmet on 22/10/2022.
+//
+
+#pragma once
+
+class Timer
+{
+
+};
---
+++ libs/src/Timer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Frederik Desmet on 22/10/2022.
+//
+
+#include "Timer.h"
