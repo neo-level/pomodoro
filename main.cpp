f2030be867c08399e3fc40275fe7ff728e1d6f00
neo-level <frederik@neo-level.com>
2022-10-22 12:08:48 +0200

    changed todo

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
  * TODO: add include library to handle time.
  * TODO: create a pomodoro class that holds the following:
  * 		- a timer
- * 		- a start time
- * 		- a stop time
+ * 		- a start function
+ * 		- a stop function
  * 		- duration options
+ * 		- a duration
  * 		- a streak
  * 		- a counter
  * 		- a reset function
