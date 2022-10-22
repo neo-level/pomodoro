8c25492b9705b8f8679324530c9fc7c0b7fdec4b
neo-level <frederik@neo-level.com>
2022-10-22 12:09:07 +0200

    defined member variables and methods.

--- inc/Pomodoro.h
+++ inc/Pomodoro.h
@@ -1,5 +1,34 @@
 //
 // Created by Frederik Desmet on 22/10/2022.
 //
-
 #pragma once
+
+#include <chrono>
+
+class Pomodoro
+{
+public:
+	Pomodoro() = default;
+	~Pomodoro() = default;
+
+	void Start();
+	void Stop();
+	void Pause();
+	void Resume();
+	void Reset();
+	void ResetCounter();
+
+	void StartBreak();
+	void EndBreak();
+
+	void AddStreak();
+	[[nodiscard]] unsigned short int GetStreak() const;
+	void AddCounter();
+	[[nodiscard]] unsigned short int GetCounter() const;
+private:
+	unsigned short int streak{};
+	unsigned short int counter{};
+	unsigned short int shorRest{ 5 };
+	unsigned short int longRest{ 15 };
+	std::chrono::minutes duration{ 25 };
+};
--- libs/src/Pomodoro.cpp
+++ libs/src/Pomodoro.cpp
@@ -1,5 +1,64 @@
 //
 // Created by Frederik Desmet on 22/10/2022.
 //
-
 #include "Pomodoro.h"
+
+void Pomodoro::Start()
+{
+
+}
+
+void Pomodoro::Stop()
+{
+
+}
+
+void Pomodoro::Pause()
+{
+
+}
+
+void Pomodoro::Resume()
+{
+
+}
+
+void Pomodoro::Reset()
+{
+
+}
+
+void Pomodoro::ResetCounter()
+{
+
+}
+
+void Pomodoro::StartBreak()
+{
+
+}
+
+void Pomodoro::EndBreak()
+{
+
+}
+
+void Pomodoro::AddStreak()
+{
+
+}
+
+unsigned short int Pomodoro::GetStreak() const
+{
+	return 0;
+}
+
+void Pomodoro::AddCounter()
+{
+
+}
+
+unsigned short int Pomodoro::GetCounter() const
+{
+	return 0;
+}
