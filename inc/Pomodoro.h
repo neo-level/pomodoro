deb74b286dfde23f57d924f0dea41a3604a894a4
neo-level <frederik@neo-level.com>
2022-10-22 12:49:19 +0200

    added break handling and expanded rest

--- inc/Pomodoro.h
+++ inc/Pomodoro.h
@@ -16,19 +16,32 @@ public:
 	void Pause();
 	void Resume();
 	void Reset();
-	void ResetCounter();
-
 	void StartBreak();
 	void EndBreak();
 
+	void StartShortBreak();
+	void StartLongBreak();
+
 	void AddStreak();
+	void ResetStreak();
 	[[nodiscard]] unsigned short int GetStreak() const;
+
 	void AddCounter();
+	void ResetCounter();
 	[[nodiscard]] unsigned short int GetCounter() const;
+	[[nodiscard]] unsigned short int GetMaxCounter() const;
+
+	void SetStartTime();
+	void SetStopTime();
 private:
 	unsigned short int streak{};
 	unsigned short int counter{};
-	unsigned short int shorRest{ 5 };
-	unsigned short int longRest{ 15 };
+	unsigned short int maxCounter{ 4 };
+
+	std::chrono::minutes shorRest{ 5 };
+	std::chrono::minutes longRest{ 15 };
 	std::chrono::minutes duration{ 25 };
+
+	std::chrono::steady_clock::time_point startTime{};
+	std::chrono::steady_clock::time_point stopTime{};
 };
--- libs/src/Pomodoro.cpp
+++ libs/src/Pomodoro.cpp
@@ -1,16 +1,22 @@
 //
 // Created by Frederik Desmet on 22/10/2022.
 //
+
 #include "Pomodoro.h"
+
+#include <thread>
+#include <iostream>
 
 void Pomodoro::Start()
 {
-
+	std::cout << "Pomodoro started" << std::endl;
+	SetStartTime();
 }
 
 void Pomodoro::Stop()
 {
-
+	std::cout << "Pomodoro ended" << std::endl;
+	SetStopTime();
 }
 
 void Pomodoro::Pause()
@@ -25,40 +31,85 @@ void Pomodoro::Resume()
 
 void Pomodoro::Reset()
 {
-
+	startTime = {};
+	stopTime = {};
+	ResetCounter();
+	ResetStreak();
 }
 
 void Pomodoro::ResetCounter()
 {
+	counter = 0;
+}
 
+void Pomodoro::ResetStreak()
+{
+	streak = 0;
 }
 
 void Pomodoro::StartBreak()
 {
+	if (GetCounter() == GetMaxCounter())
+	{
+		StartLongBreak();
+		return;
+	}
 
+	StartShortBreak();
 }
 
 void Pomodoro::EndBreak()
 {
-
+	Start();
 }
 
 void Pomodoro::AddStreak()
 {
-
+	++streak;
 }
 
 unsigned short int Pomodoro::GetStreak() const
 {
-	return 0;
+	return streak;
 }
 
 void Pomodoro::AddCounter()
 {
-
+	++counter;
 }
 
 unsigned short int Pomodoro::GetCounter() const
 {
-	return 0;
+	return counter;
 }
+
+unsigned short int Pomodoro::GetMaxCounter() const
+{
+	return maxCounter;
+}
+
+void Pomodoro::SetStartTime()
+{
+	startTime = std::chrono::steady_clock::now();
+}
+
+void Pomodoro::SetStopTime()
+{
+	stopTime = std::chrono::steady_clock::now();
+}
+
+void Pomodoro::StartShortBreak()
+{
+	Stop();
+	std::cout << "Starting short break" << std::endl;
+	std::this_thread::sleep_for(std::chrono::minutes(shorRest));
+}
+
+void Pomodoro::StartLongBreak()
+{
+	Stop();
+	std::cout << "Starting long break" << std::endl;
+	std::this_thread::sleep_for(std::chrono::minutes(longRest));
+}
+
+
