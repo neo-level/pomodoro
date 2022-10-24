34c13df14b41c590667db1211b23ee68f6410e70
neo-level <frederik@neo-level.com>
2022-10-24 16:30:50 +0200

    added comments.

--- inc/Pomodoro.h
+++ inc/Pomodoro.h
@@ -11,23 +11,73 @@ public:
 	Pomodoro() = default;
 	~Pomodoro() = default;
 
+	/**
+	 * @brief Adds a pomodoro to the counter, and increases the streak when conditions are met.
+	 */
 	void AddPomodoro();
+
+	/**
+	 * @brief Shows the pomodoro stats
+	 */
 	void ShowPomodoroStats() const;
+
+	/**
+	 * @brief Resets the pomodoro counter and streak
+	 */
 	void Reset();
 
+	/**
+	 * @brief Starts a break
+	 */
 	void StartBreak();
+
+	/**
+	 * @brief Starts a short break
+	 */
 	void StartShortBreak();
+
+	/**
+	 * @brief Starts a long break
+	 */
 	void StartLongBreak();
 
+	/**
+	 * @brief Adds a streak
+	 */
 	void AddStreak();
+
+	/**
+	 * @brief Resets the streak count.
+	 */
 	void ResetStreak();
+
+	/**
+	 * @brief Gets the streak count.
+	 * @return The streak count.
+	 */
 	[[nodiscard]] unsigned short int GetStreak() const;
 
+	/**
+	 * Adds a pomodoro to the counter.
+	 */
 	void AddCounter();
-	void ResetCounter();
-	[[nodiscard]] unsigned short int GetCounter() const;
-	[[nodiscard]] unsigned short int GetMaxCounter() const;
 
+	/**
+	 * @brief Resets the pomodoro counter.
+	 */
+	void ResetCounter();
+
+	/**
+	 * @brief Gets the pomodoro counter.
+	 * @return The pomodoro counter.
+	 */
+	[[nodiscard]] unsigned short int GetCounter() const;
+
+	/**
+	 * @brief Gets the max pomodoro count.
+	 * @return The max pomodoro count.
+	 */
+	[[nodiscard]] unsigned short int GetMaxCounter() const;
 private:
 	unsigned short int streak{};
 	unsigned short int counter{};
@@ -36,5 +86,4 @@ private:
 	std::chrono::minutes shorRest{ 5 };
 	std::chrono::minutes longRest{ 15 };
 	std::chrono::minutes pomodoro{ 25 };
-
 };
--- inc/Timer.h
+++ inc/Timer.h
@@ -9,30 +9,91 @@
 class Timer
 {
 public:
-	explicit Timer(std::chrono::minutes duration);
 	Timer() = default;
 	~Timer() = default;
 
+	/**
+	 * @brief Construct a new Timer object, with a user specified duration.
+	 *
+	 * @param duration The duration of the timer.
+	 */
+	explicit Timer(std::chrono::minutes duration);
+
+	/**
+	 * @brief Starts the timer
+	 */
 	void Start();
+
+	/**
+	 * @brief Stops the timer
+	 */
 	void Stop();
+
+	/**
+	 * @brief Resets the timer
+	 */
 	void Reset();
+
+	/**
+	 * @brief Pauses the timer
+	 */
 	void Pause();
+
+	/**
+	 * @brief Resumes the timer
+	 */
 	void Resume();
+
+	/**
+	 * @brief Prints the timer info
+	 */
 	void Info();
 
+	/**
+	 * @brief Gets the duration of the timer
+	 * @return The duration of the timer
+	 */
 	[[nodiscard]] std::chrono::minutes GetDuration() const;
+
+	/**
+	 * @brief Sets the duration of the timer
+	 */
 	void SetDuration(std::chrono::minutes duration);
 
+	/**
+	 * @brief Gets the start time of the timer
+	 * @return The start time of the timer
+	 */
 	[[nodiscard]] std::chrono::steady_clock::time_point GetStartTime() const;
+
+	/**
+	 * @brief Sets the start time of the timer
+	 */
 	void SetStartTime();
 
+	/**
+	 * @brief Gets the stop time of the timer
+	 * @return The stop time of the timer
+	 */
 	[[nodiscard]] std::chrono::steady_clock::time_point GetStopTime() const;
+
+	/**
+	 * @brief Sets the stop time of the timer
+	 */
 	void SetStopTime();
 
+	/**
+	 * @brief Gets the pause time of the timer
+	 * @return The pause time of the timer
+	 */
 	[[nodiscard]] std::chrono::minutes GetPauseTime() const;
+
+	/**
+	 * @brief Sets the pause time of the timer
+	 */
 	void SetPauseTime();
 private:
-	std::chrono::minutes _duration{25};
+	std::chrono::minutes _duration{ 25 };
 	std::chrono::minutes _pauseTime{};
 
 	std::chrono::steady_clock::time_point _startTime{};
